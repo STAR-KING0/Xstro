f0f6b41a074d8e4470bb82f974ce19b5b462f642
Astro <astrofx0011@gmail.com>
2024-07-13 13:54:56 +0200

    Update user.js

--- plugins/user.js
+++ plugins/user.js
@@ -1,18 +1,5 @@
-const os = require('os');
-const fs = require('fs');
-const Config = require('../config');
-let { fancytext, tlang, runtime, formatp, prefix, Index } = require('../lib');
-const long = String.fromCharCode(8206);
-const readmore = long.repeat(4001);
-const sᴜʜᴀɪʟ_ᴍᴅ = require('../lib/plugins');
-const util = require('util');
-const events = sᴜʜᴀɪʟ_ᴍᴅ;
-const { commands } = require('../lib');
-const { exec } = require('child_process');
-const translatte = require('translatte');
-const cron = require('node-cron');
+const { Index } = require('../lib');
 const { fetchCategory, getallMenu, listCommandDesc, sendOwnerContact } = require('../mods');
-var cronStart = false;
 Index(
   {
     pattern: 'help',
