4b8b7b667550860e9a6837eb5ca2ec5671ba517b
Astro <astrofx0011@gmail.com>
2024-07-16 13:25:34 +0200

    remove useless vars

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 const fs = require('fs-extra');
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname + '/config.env' });
 
+global.devs = '2348039607375';
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : 'null';
+global.owner = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : '2348039607375';
+
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || '';
 global.email = 'samsamsun789@gmail.com';
@@ -11,15 +15,11 @@ global.allowJids = process.env.ALLOW_JID || 'null';
 global.blockJids = process.env.BLOCK_JID || 'null';
 global.DATABASE_URL = process.env.DATABASE_URL || '';
 
-global.timezone = process.env.TZ || process.env.TIME_ZONE || 'Asia/Karachi';
+global.timezone = process.env.TIME_ZONE || 'Asia/Karachi';
 global.github = process.env.GITHUB || 'https://github.com/SuhailTechInfo/Suhail-Md';
 global.gurl = process.env.GURL || 'https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643';
 global.website = process.env.GURL || 'https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643';
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || 'https://telegra.ph/file/d5b1c3544fedc23e11a06.jpg';
-
-global.devs = '2348039607375'; // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : 'null';
-global.owner = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : '2348039607375';
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/d5b1c3544fedc23e11a06.jpg';
 
 global.style = process.env.STYLE || '0';
 global.flush = process.env.FLUSH || 'false';
@@ -28,19 +28,16 @@ global.wlcm = process.env.WELCOME || 'false';
 
 global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || 'false';
-(global.disablegroup = process.env.DISABLE_GROUPS || 'false'), (global.MsgsInLog = process.env.MSGS_IN_LOG || 'false');
+global.disablegroup = process.env.DISABLE_GROUPS || 'false';
 global.userImages = process.env.USER_IMAGES || '';
 global.waPresence = process.env.WAPRESENCE || 'set according to your need';
 global.readcmds = process.env.READ_COMMAND || 'false';
 global.readmessage = process.env.READ_MESSAGE || 'false';
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || '2348039607375,923xxxxxxxx';
 global.read_status = process.env.AUTO_READ_STATUS || 'false';
 global.save_status = process.env.AUTO_SAVE_STATUS || 'false';
-global.save_status_from = process.env.SAVE_STATUS_FROM || 'null,923xxxxxxxx';
-global.read_status_from = process.env.READ_STATUS_FROM || '2348039607375,923xxxxxxxx';
+global.save_status_from = process.env.SAVE_STATUS_FROM || 'null';
+global.read_status_from = process.env.READ_STATUS_FROM || 'null';
 
-global.api_smd = 'https://api-smd-1.vercel.app';
-global.scan = 'https://suhail-md-vtsf.onrender.com/';
 
 global.SESSION_ID = process.env.SESSION_ID || '';
 
